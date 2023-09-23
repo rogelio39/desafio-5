4992fc0f65b72d1d6f4726b8d5f42cf807c0e137
Rogelio Suleta <andresrogesu@gmail.com>
2023-09-23 16:57:46 -0300

    se agrego la ruta delete y put a carts models

--- src/routes/cart.models.routes.js
+++ src/routes/cart.models.routes.js
@@ -44,15 +44,15 @@ cartModelsRouter.post('/:cid/product/:pid', async (req, res) => {
             const product = await productModel.findById(pid);
             if (product) {
                 //chequeamos si el producto existe en el carrito
-                const index = cart.products.findIndex(prod => prod.id_prod.toString() === pid);
+                const index = cart.products.findIndex(prod => prod.id_prod._id.toString() === pid);
                 if (index != -1) {
                     cart.products[index].quantity += quantity;
                 } else {
                     cart.products.push({ id_prod: pid, quantity: quantity });
                 }
                 //actualizamos el carrito
-                const respuesta = await cartModel.findByIdAndUpdate(cid, {products : cart.products });
-                res.status(200).send({ respuesta: 'ok', mensaje: respuesta});
+                const respuesta = await cartModel.findByIdAndUpdate(cid, { products: cart.products });
+                res.status(200).send({ respuesta: 'ok', mensaje: respuesta });
 
             } else {
                 res.status(404).send({ respuesta: 'error al agregar producto al carrito', mensaje: 'product not found' });
@@ -64,6 +64,77 @@ cartModelsRouter.post('/:cid/product/:pid', async (req, res) => {
         res.status(500).send({ respuesta: 'error al agregar producto al carrito', mensaje: error })
     }
 })
+
+
+cartModelsRouter.delete('/:cid/product/:pid', async (req, res) => {
+    try {
+        const { cid, pid } = req.params;
+        //chequeamos si el carrito existe y almacenamos ese valor en la variable cart
+        const cart = await cartModel.findById(cid)
+        if (cart) {
+            //chequeamos si el producto existe en la base de datos.
+            const product = await productModel.findById(pid);
+            if (product) {
+                //chequeamos si el producto existe en el carrito
+                const index = cart.products.findIndex(prod => prod.id_prod._id.toString() === pid);
+                if (index != -1) {
+                    cart.products.splice(index, 1);
+                    //actualizamos el carrito
+                    const respuesta = await cartModel.findByIdAndUpdate(cid, { products: cart.products });
+                    res.status(200).send({ respuesta: 'producto eliminado', mensaje: respuesta });
+                } else {
+                    res.status(404).send({ respuesta: 'error', mensaje: 'producto no existente, error al tratar de borrarlo' })
+                }
+            } else {
+                res.status(404).send({ respuesta: 'error al agregar eliminar el producto del carrito', mensaje: 'product not found' });
+            }
+        } else {
+            res.status(404).send({ respuesta: 'error al eliminar producto del carrito', mensaje: 'Carrito no existe' });
+        }
+    } catch (error) {
+        res.status(500).send({ respuesta: 'error al eliminar producto del carrito', mensaje: error })
+    }
+})
+
+
+cartModelsRouter.put('/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
+        const { products } = req.body;
+
+        //chequeamos si el carrito existe y almacenamos ese valor en la variable cart
+        const cart = await cartModel.findById(cid);
+        if (!cart) {
+            return res.status(404).send({ respuesta: 'error al agregar producto al carrito', mensaje: 'Carrito no existe' });
+        }
+
+        const productPromises = products.map(async (prod) => {
+            const product = await productModel.findById(prod._id);
+            if (!product) {
+                throw new Error(`Producto no encontrado: ${prod._id}`);
+            }
+            const index = cart.products.findIndex(cartProd => cartProd._id.toString() === product._id);
+            if (index != -1) {
+                throw new Error(`Producto ya existente: ${product._id.toString()}`);
+            }
+            return {id_prod: product._id};
+        });
+
+        try {
+            // Esperamos a que se completen todas las promesas
+            const productResults = await Promise.all(productPromises);
+            // Si todos los productos se encontraron, actualizamos el carrito
+            cart.products = productResults;
+            const respuesta = await cartModel.findByIdAndUpdate(cid, { products: cart.products });
+            res.status(200).send({ respuesta: 'ok', mensaje: respuesta });
+        } catch (error) {
+            res.status(404).send({ respuesta: 'error', mensaje: 'error al cargar array de productos' });
+        }
+    } catch (error) {
+        res.status(500).send({ respuesta: 'error al agregar producto al carrito', mensaje: error.message })
+    }
+});
+
 
 
 export default cartModelsRouter;
